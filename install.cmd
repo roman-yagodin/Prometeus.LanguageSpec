0ee5bfc269931dd5b0d07fb0162f0333d91dd419
roman-yagodin <roman.yagodin@gmail.com>
2014-10-10 11:40:30 +0400

    Allow install for GEdit 3

--- install.cmd
+++ install.cmd
@@ -1,9 +1,24 @@
 @echo off
 
 IF EXIST "%PROGRAMFILES(x86)%\gedit" (
-   xcopy prometeus.lang "%PROGRAMFILES(x86)%\gedit\share\gtksourceview-2.0\language-specs" /-Y
-) ELSE IF EXIST "%PROGRAMFILES%\gedit" ( 
-   xcopy prometeus.lang "%PROGRAMFILES%\gedit\share\gtksourceview-2.0\language-specs" /-Y
-) ELSE (
-   echo "Please install GEdit first."
+	rem Install for GEdit 2 in Windows 64-bit
+	xcopy prometeus.lang "%PROGRAMFILES(x86)%\gedit\share\gtksourceview-2.0\language-specs\" /Y /Q
+	echo Installed for GEdit 2
+) 
+
+IF EXIST "%PROGRAMFILES%\gedit" ( 
+	IF EXIST "%PROGRAMFILES%\gedit\share\gtksourceview-2.0" (
+		rem Install for GEdit 2 in Windows 32-bit
+		xcopy prometeus.lang "%PROGRAMFILES%\gedit\share\gtksourceview-2.0\language-specs\" /Y /Q
+		echo Installed for GEdit 2
+		goto :DONE
+	) ELSE IF EXIST "%PROGRAMFILES%\gedit\share\gtksourceview-3.0" (
+		rem Install for GEdit 3 in Windows 64-bit
+		xcopy prometeus.lang "%PROGRAMFILES%\gedit\share\gtksourceview-3.0\language-specs\" /Y /Q
+		echo Installed for GEdit 3
+		goto :DONE
+	)
 )
+
+echo Not installed, something wrong...
+:DONE
